9a396e7f9957c8fb0959ab9b4445fa2fc40f637e
Gustavo <guto.h.d@hotmail.com>
2022-09-07 17:36:54 -0300

    Adicionando outras classe-relacionando e upando db

--- Data/CSharpUdemy_MVCContext.cs
+++ Data/CSharpUdemy_MVCContext.cs
@@ -9,11 +9,15 @@ namespace CSharpUdemy_MVC.Data
 {
     public class CSharpUdemy_MVCContext : DbContext
     {
-        public CSharpUdemy_MVCContext (DbContextOptions<CSharpUdemy_MVCContext> options)
+        public CSharpUdemy_MVCContext(DbContextOptions<CSharpUdemy_MVCContext> options)
             : base(options)
         {
         }
 
-        public DbSet<CSharpUdemy_MVC.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+
+        public DbSet<Seller> Seller { get; set; } = default!;
+
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
     }
 }
---
+++ Models/Enums/SaleStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpUdemy_MVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+    
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
