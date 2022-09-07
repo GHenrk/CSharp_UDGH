3e6bb0e4d26294566fc6af7c80cef3fd879a3f2b
Gustavo <guto.h.d@hotmail.com>
2022-09-07 19:35:13 -0300

    Criando controller de Vendedores

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpUdemy_MVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
