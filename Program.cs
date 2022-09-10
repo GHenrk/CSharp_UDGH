849487e2d31d9a07b810134568fcf71227152fbb
Gustavo <guto.h.d@hotmail.com>
2022-09-10 15:28:36 -0300

    Formatando datas

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Globalization;
+
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using CSharpUdemy_MVC.Data;
 using Pomelo.EntityFrameworkCore.MySql;
 using CSharpUdemy_MVC.Services;
+using Microsoft.Extensions.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<CSharpUdemy_MVCContext>(options =>
-    options.UseMySql("server=localhost;user=root;password=1234567; database=CSharpUdemy" , MySqlServerVersion.Parse("8.0.30-mysql")));
+    options.UseMySql("server=localhost;user=root;password=1234567; database=CSharpUdemy", MySqlServerVersion.Parse("8.0.30-mysql")));
 
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -23,13 +26,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    
-   
+
+
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 SeedDatabase();
+LocalizationService();
 
 app.UseRouting();
 
@@ -48,5 +52,20 @@ void SeedDatabase() //Cria uma função
     {
         var dbInitializer = scope.ServiceProvider.GetRequiredService<SeedingService>();
         dbInitializer.Seed(); //Roda a funcao do seedingService
+    }
+}
+
+void LocalizationService()
+{
+    using (var scope1 = app.Services.CreateScope())
+    {
+        var enUs = new CultureInfo("en-US");
+        var localizationOptions = new RequestLocalizationOptions
+        {
+            DefaultRequestCulture = new RequestCulture(enUs),
+            SupportedCultures = new List<CultureInfo> { enUs },
+            SupportedUICultures = new List<CultureInfo> { enUs },
+        };
+        app.UseRequestLocalization(localizationOptions);
     }
 }
